87458f6d351e8d5ae3e08d29bcd406153b15aae7
Vinicius Brunoni <viniciusbrunoni8@gmail.com>
2024-10-10 02:52:53 -0300

    Adicionando função reiniciar

--- js/app.js
+++ js/app.js
@@ -36,6 +36,8 @@ function embaralha(lista){
 }
 
 function reiniciar(){
-
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 
 }

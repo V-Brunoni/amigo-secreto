f9492e3ef84353c780c92686ffcd34575221f02b
Vinicius Brunoni <viniciusbrunoni8@gmail.com>
2024-10-10 02:48:00 -0300

    Adicionando funções sortear e embaralha

--- js/app.js
+++ js/app.js
@@ -1,19 +1,38 @@
+let amigos = [];
+
 function adicionar(){
     let nomeAmigo = document.getElementById('nome-amigo');
     let listaAmigos = document.getElementById('lista-amigos');
-
+    amigos.push(nomeAmigo.value);
     if (listaAmigos.innerHTML == '') {
         listaAmigos.innerHTML = nomeAmigo.value;
     } else {
         listaAmigos.innerHTML = listaAmigos.innerHTML + '<br>' + nomeAmigo.value;
     }
-
     nomeAmigo.value = '';
 }
 
 function sortear(){
+    embaralha(amigos);
+    let listaSorteio = document.getElementById('lista-sorteio');
 
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            listaSorteio.innerHTML = listaSorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
+        } else {
+            listaSorteio.innerHTML = listaSorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+        }
+    }
 
+}
+
+function embaralha(lista){
+    for (let indice = lista.length; indice; indice--){
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
 
 function reiniciar(){
